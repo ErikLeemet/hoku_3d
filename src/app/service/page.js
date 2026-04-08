6c49639c688b9b1ace6672b293edc7c04532f1bd
erik <erik.leemet@gmail.com>
2026-04-08 23:11:10 +0300

    Services layout base

--- src/app/service/page.js
+++ src/app/service/page.js
@@ -1,51 +1,26 @@
 import React from "react";
 
 const Service = () => {
-  return (
-    <div id="services" className="flex relative flex-col items-center justify-center font-thedusCLR rounded-b-[30px] min-h-screen sm:min-h-[600px] md:min-h-[700px] lg:min-h-[860px] bg-surface px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-16">
-      <div className="flex w-full max-w-7xl mx-auto items-center justify-center">
-        <div className="py-6 sm:py-8 lg:py-16 w-full">
-          <div className="space-y-6 sm:space-y-8 lg:grid md:grid-cols-3 lg:space-y-0 lg:gap-6 xl:gap-8">
-            <div className="flex max-w-lg space-y-6 sm:space-y-8 items-start flex-col rounded-3xl bg-panel p-6 sm:p-7 lg:p-8">
-              <div className="my-4 sm:my-6 lg:my-8 flex w-full items-baseline justify-center">
-                <span className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-thedusWLB text-body text-center">
-                  3D PRINTIMINE
-                </span>
-              </div>
+    return (
+        <div id="services" className="grid grid-cols-[1fr_1fr_1fr_auto_1fr_1fr_1fr] grid-rows-3 place-items-center items-center justify-center font-thedusCLR rounded-b-[30px] min-h-screen sm:min-h-[600px] md:min-h-[700px] lg:min-h-[860px] bg-surface px-4 sm:px-6 lg:px-8 pb-8 sm:pb-12 lg:pb-16">
+                <div className="col-start-2 row-start-1 bg-gray-300 p-4">
+                    Item 1
+                </div>
 
-              <p className="w-full text-sm sm:text-base lg:text-lg text-muted">
-                Protsess, mille käigus luuakse digitaalfaili põhjal kiht-kiht haaval kolmemõõtmelisi objekte, kasutades selleks materjale nagu plast, vaik või metall.
-              </p>
-            </div>
-            <div className="flex max-w-lg space-y-6 sm:space-y-8 items-start flex-col rounded-3xl bg-panel p-6 sm:p-7 lg:p-8">
-              <div className="my-4 sm:my-6 lg:my-8 flex w-full items-baseline justify-center">
-                <span className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-thedusWLB text-body text-center">
-                  GRAVEERIMINE
-                </span>
-              </div>
+                <div className="col-start-6 row-start-2 bg-gray-300 p-4">
+                    Item 2
+                </div>
 
-              <p className="w-full text-sm sm:text-base lg:text-lg text-muted">
-                Motiivide, teksti või mustrite graveerimine sellistele pindadele nagu metall, puit või klaas.
-              </p>
-            </div>
-            <div className="flex max-w-lg space-y-6 sm:space-y-8 items-start flex-col rounded-3xl bg-panel p-6 sm:p-7 lg:p-8">
-              <div className="my-4 sm:my-6 lg:my-8 flex w-full items-baseline justify-center">
-                <span className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-thedusWLB text-body text-center">
-                  3D-modelleerimine
-                </span>
-              </div>
-
-              <p className="w-full text-2xl sm:text-base lg:text-2xl text-muted text-center">
-                VARSTI TULEKUL!
-              </p>
-            </div>
-          </div>
+                <div className="col-start-2 row-start-3 bg-gray-300 p-4">
+                    Item 3
+                </div>
+                {/* Decoration */}
+                <div className="col-start-4 row-start-1 row-span-4 w-[6px] h-full bg-neutral"></div>
+                <div className="col-start-3 row-start-1 bg-neutral w-full h-[6px]"></div>
+                <div className="col-start-5 row-start-2 bg-neutral w-full h-[6px]"></div>
+                <div className="col-start-3 row-start-3 bg-neutral w-full h-[6px]"></div>
         </div>
-      </div>
-      <div className="w-4/5 sm:w-3/4 md:w-1/2 h-[40px] sm:h-[50px] lg:h-[60px] bottom-0 absolute rounded-t-[30px] bg-surface-2">
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Service;
