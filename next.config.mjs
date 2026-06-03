287940c9dd4975424d66756b5caf58189e34b320
erik <erik.leemet@gmail.com>
2026-06-03 13:14:59 +0300

    overall fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    root: "/Users/erikleemet/coding/new_hoku/hoku_3d",
+  },
+};
 
 export default nextConfig;
